6556e886cc16134daa665abc7e78db183586ca2f
MrMarrant <laughymclaugher@gmail.com>
2023-06-18 17:09:47 +0200

    [FEATURE] Add link section

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,41 +1,58 @@
-import './styles/portfolio.scss';
+import "./styles/portfolio.scss";
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-window.addEventListener('load', (event) => {
-    let i = 0
-    let titleValue = document.getElementById("title_name");
-    const interval = setInterval(() => {
-        titleValue.innerText = titleValue.innerText.split("").map((letter, index) => {
-            if (index < i) return titleValue.dataset.value[index];
-            return letters[Math.floor(Math.random() * 26)];
-        }).join("");
-        if (i >= 9) clearInterval(interval)
-        i += 1 / 3;
-    }, 30);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+window.addEventListener("load", (event) => {
+  let i = 0;
+  let titleValue = document.getElementById("title_name");
+  const interval = setInterval(() => {
+    titleValue.innerText = titleValue.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < i) return titleValue.dataset.value[index];
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+    if (i >= 9) clearInterval(interval);
+    i += 1 / 3;
+  }, 30);
 });
 
-window.addEventListener('scroll', function (event) {
-    let i = 0
+window.addEventListener(
+  "scroll",
+  function (event) {
+    let i = 0;
     let titleValue = document.getElementById("description_job");
     const interval = setInterval(() => {
-        titleValue.innerText = titleValue.dataset.value.split("").map((letter, index) => {
-            if (index < i) return titleValue.dataset.value[index];
-            return letters[Math.floor(Math.random() * 26)];
-        }).join("");
-        if (i >= 25) clearInterval(interval)
-        i += 1 / 2;
+      titleValue.innerText = titleValue.dataset.value
+        .split("")
+        .map((letter, index) => {
+          if (index < i) return titleValue.dataset.value[index];
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+      if (i >= 25) clearInterval(interval);
+      i += 1 / 2;
     }, 30);
-}, {once : true});
+  },
+  { once: true }
+);
 
-window.addEventListener('scroll', function (event) {
-    let i = 0
+window.addEventListener(
+  "scroll",
+  function (event) {
+    let i = 0;
     let titleValue = document.getElementById("description_hobbie");
     const interval = setInterval(() => {
-        titleValue.innerText = titleValue.dataset.value.split("").map((letter, index) => {
-            if (index < i) return titleValue.dataset.value[index];
-            return letters[Math.floor(Math.random() * 26)];
-        }).join("");
-        if (i >= 20) clearInterval(interval)
-        i += 1 / 3;
+      titleValue.innerText = titleValue.dataset.value
+        .split("")
+        .map((letter, index) => {
+          if (index < i) return titleValue.dataset.value[index];
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+      if (i >= 20) clearInterval(interval);
+      i += 1 / 3;
     }, 30);
-}, {once : true});
+  },
+  { once: true }
+);
