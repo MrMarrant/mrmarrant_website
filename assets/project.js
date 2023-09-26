7564a2485d78803380a8b0f31d9d86391109d3ae
MrMarant <laughymclaugher@gmail.com>
2023-09-26 20:01:22 +0200

    [FEATURE] Add home & arrow to up button

--- assets/project.js
+++ assets/project.js
@@ -1,1 +1,18 @@
 import './styles/project.scss';
+
+const scrollToTopBtn = document.getElementById('arrow');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
